67af81735bc2bd95e01637e64255f60fd7e87ede
Rafael Cavalcanti <rccavalcanti@gmail.com>
2019-09-13 21:54:21 -0300

    Add removal of duplicate entries

--- app/book.rb
+++ app/book.rb
@@ -5,12 +5,13 @@ class Book
   def initialize(title, author)
     @title = title
     @author = author
-    @entries = []
     set_filename
+    @entries = []
   end
 
   def clean_book
     rm_empty_entries
+    rm_dup_entries
   end
 
 
@@ -25,5 +26,14 @@ class Book
 
   def rm_empty_entries
     @entries.select! { |entry| entry.text.strip != "" }
+  end
+
+  def rm_dup_entries
+    # We shouldn't remove notes with equal text, as they can be desired
+    # Eg: TODO reminders on a document
+    # Duplicate highlights, on the other hand, are probably useless
+    @entries.uniq! do |entry|
+      [entry.text, entry.type != Entry::TYPE_NOTE || entry.desc]
+    end
   end
 end
