08de4b6a1d0a929b3cb4d6188ad8ae3eece7e5e0
Rafael Cavalcanti <dev@rafaelc.org>
2022-02-13 23:45:20 -0300

    Refactor moving more logic from class to template

--- lib/fyodor/output_generator.rb
+++ lib/fyodor/output_generator.rb
@@ -18,55 +18,18 @@ module Fyodor
     private
 
     def regular_entries
-      @book.reject { |entry| entry.type == Entry::TYPE[:bookmark] }
+      @regular_entries ||= @book.reject { |entry| entry.type == Entry::TYPE[:bookmark] }.sort
     end
 
     def bookmarks
-      @book.select { |entry| entry.type == Entry::TYPE[:bookmark] }
+      @bookmarks ||= @book.select { |entry| entry.type == Entry::TYPE[:bookmark] }.sort
     end
 
-    def render_entries(entries)
-      output = ""
-      entries.sort.each do |entry|
-        output += "- #{item_text(entry)}\n\n"
-        output += "  #{item_desc(entry)}\n\n" unless item_desc(entry).empty?
-      end
-      output.strip
-    end
+    def place(entry)
+      raise "Description is not parsed." unless entry.desc_parsed?
 
-    def item_text(entry)
-      case entry.type
-      when Entry::TYPE[:bookmark]
-        "#{page(entry)}"
-      when Entry::TYPE[:note]
-        "_Note:_ #{entry.text.strip}"
-      else
-        "#{entry.text.strip}"
-      end
-    end
-
-    def item_desc(entry)
-      return entry.desc unless entry.desc_parsed?
-
-      case entry.type
-      when Entry::TYPE[:bookmark]
-        time(entry)
-      else
-        (type(entry) + " @ " + page(entry) + " " + time(entry)).strip
-      end
-    end
-
-    def page(entry)
       ((entry.page.nil? ? "" : "page #{entry.page}, ") +
         (entry.loc.nil? ? "" : "loc. #{entry.loc}")).delete_suffix(", ")
-    end
-
-    def time(entry)
-      @config["time"] ? "[#{entry.time}]" : ""
-    end
-
-    def type(entry)
-      SINGULAR[entry.type]
     end
   end
 end
