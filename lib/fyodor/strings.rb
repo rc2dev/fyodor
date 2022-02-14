a909affb470b5df7e4e7886e3b4947ee43d90808
Rafael Cavalcanti <dev@rafaelc.org>
2022-02-14 18:08:11 -0300

    Minor formatting

--- lib/fyodor/strings.rb
+++ lib/fyodor/strings.rb
@@ -2,17 +2,21 @@ require "fyodor/entry"
 
 module Fyodor
   module Strings
-    PLURAL = { Entry::TYPE[:highlight] => "highlights",
-               Entry::TYPE[:note] => "notes",
-               Entry::TYPE[:bookmark] => "bookmarks",
-               Entry::TYPE[:clip] => "clips",
-               nil => "unrecognized" }
+    PLURAL = {
+      Entry::TYPE[:highlight] => "highlights",
+      Entry::TYPE[:note] => "notes",
+      Entry::TYPE[:bookmark] => "bookmarks",
+      Entry::TYPE[:clip] => "clips",
+      nil => "unrecognized"
+    }
 
-    SINGULAR = { Entry::TYPE[:highlight] => "highlight",
-                 Entry::TYPE[:note] => "note",
-                 Entry::TYPE[:bookmark] => "bookmark",
-                 Entry::TYPE[:clip] => "clip",
-                 nil => "unrecognized",
-                 :AUTHOR_NA => "Author Not Available"}
+    SINGULAR = {
+      Entry::TYPE[:highlight] => "highlight",
+      Entry::TYPE[:note] => "note",
+      Entry::TYPE[:bookmark] => "bookmark",
+      Entry::TYPE[:clip] => "clip",
+      nil => "unrecognized",
+      :AUTHOR_NA => "Author Not Available"
+    }
   end
 end
